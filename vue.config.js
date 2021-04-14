a57fda262d06bab67ed924f5cb0e96e4cc598fe3
jinwan-zhejin <jin.wan@zhejintech.com>
2021-04-14 09:49:07 +0800

    修改当前查询结果

--- vue.config.js
+++ vue.config.js
@@ -42,11 +42,11 @@ module.exports = {
         ws: false,
         // target: `http://localhost:${apiPort}/api`,
         // target: 'http://47.103.79.104:9090',
-        target: 'http://183.194.64.166:9528',
+        // target: 'http://183.194.64.166:9528',
         // target: 'http://192.168.3.83:8080', // 曹海生
         // target: 'http://192.168.3.70:8083', // 刘向前
         // target: 'http://192.168.3.39:8080', // 侯峰
-        // target: 'http://192.168.3.108:8080', // 卢兰杰
+        target: 'http://192.168.3.108:8080', // 卢蓝杰
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_API]: '/' + process.env.VUE_APP_API
