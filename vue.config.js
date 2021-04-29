7335819c34efbec9ab0a303b329c62b89a0a4003
wanghao <hao.wang@zhejintech.com>
2021-04-29 14:08:11 +0800

    projectGroupId传值问题_01

--- vue.config.js
+++ vue.config.js
@@ -42,13 +42,13 @@ module.exports = {
         ws: false,
         // target: `http://localhost:${apiPort}/api`,
         // target: 'http://47.103.79.104:9090',
-        target: 'http://183.194.64.166:9527',
+        // target: 'http://183.194.64.166:9527',
         // target: 'http://192.168.3.115:8080', // 杨江主
         // target: 'http://192.168.3.83:8080', // 曹海生
         // target: 'http://192.168.3.70:8083', // 刘向前
         // target: 'http://192.168.3.39:8080', // 侯峰
         // target: 'http://192.168.3.108:8080', // 卢兰杰
-        // target: 'http://192.168.3.117:8080', // 贺建立
+        target: 'http://192.168.3.117:8080', // 贺建立
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_API]: '/' + process.env.VUE_APP_API
